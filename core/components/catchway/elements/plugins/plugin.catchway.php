a92d72f1f105a5abe28244171fbaf9487720eefb
me6iaton <me6iaton@gmail.com>
2015-02-22 15:32:15 +0300

    fix find new city in context

--- core/components/catchway/elements/plugins/plugin.catchway.php
+++ core/components/catchway/elements/plugins/plugin.catchway.php
@@ -2,17 +2,17 @@
 /* @var Catchway $Catchway */
 $Catchway = $modx->getService('catchway', 'Catchway', $modx->getOption('catchway_core_path', null, $modx->getOption('core_path') . 'components/catchway/') . 'model/catchway/');
 
+$context = $modx->context->key;
+if ($context == 'mgr') return;
+
 switch ($modx->event->name) {
   case 'OnHandleRequest':
-    $context = $modx->context->key;
-    if ($context == 'mgr') break;
-
     $cookieNameId = 'catchway-city-' . $context . '-id';
     $time = time() + 60 * 60 * 24 * 60;
     $parts = array_reverse(explode('.', $_SERVER['SERVER_NAME']));
     $domain = $parts[1] . '.' . $parts[0];
 
-    // set city
+    // set city, if $_REQUEST['catchway_city']
     if ($city = $_REQUEST['catchway_city']) {
       if ($city == 'default') {
         $id = $modx->getOption('catchway_default_page');
@@ -26,7 +26,7 @@ switch ($modx->event->name) {
         $contexts = $Catchway->pdoTools->run();
         foreach ($contexts as $item) {
           if ($item['key'] == 'mgr') continue;
-          setcookie('catchway-city-' . $item['key'] . '-id', '', time() - 3600);
+          setcookie('catchway-city-' . $item['key'] . '-id', '', time() - 3600, null, $domain);
         }
         // get city id
         $id = $Catchway->getSityId($context, $city);
@@ -48,38 +48,43 @@ switch ($modx->event->name) {
     // redirect to city page
     if ($_SERVER['REQUEST_URI'] == '/') {
       if (isset($_COOKIE[$cookieNameId])) {
-        // prevent a redirect loop
-        if ($_COOKIE[$cookieNameId] !== '1') {
+        if ($url = $modx->makeUrl($_COOKIE[$cookieNameId], $context) == '/') {
+          break; // prevent a redirect loop
+        } else {
           $modx->sendRedirect($modx->makeUrl($_COOKIE[$cookieNameId]));
           break;
         }
       } else if (isset($_COOKIE['catchway-city-name'])) {
-        // get city id
+        // if $cookieNameId not found in this context, get city id
         $id = $Catchway->getSityId($context, $_COOKIE['catchway-city-name']);
-        if($id){
+        if ($id) {
           $modx->sendRedirect($modx->makeUrl($id));
           break;
-        }else{
-          if($id = $modx->getOption('catchway_default_page')){
-            $modx->sendRedirect($modx->makeUrl($id));
-            break;
+        } else {
+          // redirect default city page
+          if ($id = $modx->getOption('catchway_default_page')) {
+            $res = $modx->getObject('modResource', array('id' => $id, 'context_key' => $context));
+            if ($res) {
+              $modx->sendRedirect($modx->makeUrl($id, $context));
+              break;
+            }
           }
         }
-      }else {
-        if ($catchwayJs = $modx->getOption('catchway_frontend_js')) {
-          $cities = $Catchway->getCities(array('tpl'=>'tpl.catchway.modal.cities.row'));
-          $output = $Catchway->getChunk('tpl.catchway.modal', array('output' => $cities), true);
-          $modx->regClientHTMLBlock($output);
-          $modx->regClientScript($modx->getOption('catchway_assets_url') . 'vendor/curl/dist/curl-with-js-and-domReady/curl.js');
-          $modx->regClientScript($catchwayJs);
-          break;
-        }
+      }
+
+      // show modal dialog, if sity not found before this
+      if ($catchwayJs = $modx->getOption('catchway_frontend_js')) {
+        $cities = $Catchway->getCities(array('tpl' => 'tpl.catchway.modal.cities.row'));
+        $output = $Catchway->getChunk('tpl.catchway.modal', array('output' => $cities), false);
+        $modx->regClientHTMLBlock($output);
+        $modx->regClientScript($modx->getOption('catchway_assets_url') . 'vendor/curl/dist/curl-with-js-and-domReady/curl.js');
+        $modx->regClientScript($catchwayJs);
+        break;
       }
     }
     break;
 
   case 'OnUserActivate':
-    if ($context == 'mgr') break;
     // update user
     if (isset($_COOKIE['catchway-city-name'])){
       if ($profile = $modx->user->getOne('Profile')) {
